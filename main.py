7d1f19f4b25771cfe832b128e459b6611dbc49a8
Franco <francoarturi@gmail.com>
2024-08-27 18:56:32 -0300

    Modularizacion del programa

---
+++ main.py
@@ -0,0 +1,15 @@
+def crearTarea():
+def eliminarTarea():
+def cargarTarea():
+def completarTarea():
+def guardarProgresoTareas():
+def buscarTarea():
+def cargarUsuario():
+def cambiarUsuario():
+
+
+#MAIN
+estado=True
+while estado==True:
+    #MENU DONDE VAMOS A SELECCIONAR PRIMERO UN USUARIO.
+        #UNA VEZ CARGADO EL USUARIO, OFRECEMOS LAS OPCIONES DEL GESTOR DE TAREAS.
