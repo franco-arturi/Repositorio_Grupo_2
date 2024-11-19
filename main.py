935dca3a4a58ef4c45f68192ca1c4868081835b3
Franco <francoarturi@gmail.com>
2024-11-19 18:33:24 -0300

    Cambie un par de comentarios.

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ ARCHIVO_LOG = 'actividad.log'
 
 # Función para cargar datos desde el archivo JSON
 def cargar_datos():
+    """
+    Objetivo: Se cargan o crean datos del archivo JSON.
+    Parametros de Entrada: La variable global con el nombre del archivo JSON.
+    Parametros de Salida: Se devuelven los datos del archivo JSON.
+    """
     try:
         with open(ARCHIVO_JSON, 'r') as archivo:
             datos = json.load(archivo)
@@ -32,12 +37,22 @@ def cargar_datos():
 
 # Función para guardar datos en el archivo JSON
 def guardar_datos(datos):
+    """
+    Objetivo: Se llama cuando se quiere guardar los datos en el archivo JSON.
+    Parametros de Entrada: Ingresamos los datos del archivo.
+    Parametros de Salida: Si bien no hay ninguno return, se actualiza el JSON.
+    """
     with open(ARCHIVO_JSON, 'w') as archivo:
         json.dump(datos, archivo, indent=4)
     print("Datos guardados exitosamente.")
 
 # Función para registrar actividad en el log
 def registrar_actividad(mensaje):
+    """
+    Objetivo: Se registra la actividad en un archivo.
+    Parametros de Entrada: Entra el mensaje de actividad como parametro.
+    Parametros de Salida: Si bien no hay un return, se ingresan los datos al archivo.
+    """
     with open(ARCHIVO_LOG, 'a') as log_file:
         timestamp = d.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log_file.write(f"{timestamp} - {mensaje}\n")
